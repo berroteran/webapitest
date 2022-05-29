a331842d5bdc18329967f571e3efd07d01ee6572
Omar Berroterán Silva <omarberroteranlkf@gmail.com>
2022-05-29 13:07:58 -0600

    Initial commit

---
+++ src/main/java/com/examen/webapitest/WebapitestApplication.java
@@ -0,0 +1,13 @@
+package com.examen.webapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapitestApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run( WebapitestApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/examen/webapitest/WebapitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.webapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapitestApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
