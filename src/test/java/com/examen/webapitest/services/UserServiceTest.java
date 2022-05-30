a425d84a299094be770b1384bf77fc29696aacea
Omar Berroterán Silva <omarberroteranlkf@gmail.com>
2022-05-30 04:25:03 -0600

    add test template

---
+++ src/test/java/com/examen/webapitest/services/UserServiceTest.java
@@ -0,0 +1,25 @@
+package com.examen.webapitest.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp () {
+    }
+
+    @Test
+    void findAll () {
+    }
+
+    @Test
+    void findById () {
+    }
+
+    @Test
+    void save () {
+    }
+}
---
+++ src/test/java/com/examen/webapitest/weapi/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.examen.webapitest.weapi;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp () {
+    }
+
+    @AfterEach
+    void tearDown () {
+    }
+
+    @Test
+    void findAllUsers () {
+    }
+
+    @Test
+    void findUserById () {
+    }
+
+    @Test
+    void saveUser () {
+    }
+
+    @Test
+    void all () {
+    }
+}
